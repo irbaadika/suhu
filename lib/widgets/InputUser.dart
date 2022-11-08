b304fa86993d2855358f8e6a725f7011e1b174a1
irbaadika <irbaadika123@gmail.com>
2022-11-08 10:58:27 +0700

    commit1

---
+++ lib/widgets/InputUser.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class InputUser extends StatelessWidget {
+  const InputUser({
+    Key? key,
+    required this.input_user,
+  }) : super(key: key);
+
+  final TextEditingController input_user;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: input_user,
+      keyboardType: TextInputType.number,
+      inputFormatters: <TextInputFormatter>[
+        FilteringTextInputFormatter.digitsOnly
+      ],
+      decoration: InputDecoration(
+          labelText: "Celcius", hintText: "Masukkan Suhu Dalam Celcius"),
+    );
+  }
+}
